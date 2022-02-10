4c5d7ddf0ad27f6c9259bc87c7917355d0450bbc
tutumenezes <tutumenezes@hub9.co>
2022-02-10 04:33:21 -0300

    parsing correct date without days

--- src/lib/blog-helpers.ts
+++ src/lib/blog-helpers.ts
@@ -2,10 +2,9 @@ export const getBlogLink = (slug: string) => {
   return `/blog/${slug}`
 }
 
-export const getDateStr = date => {
+export const getDateStr = (date) => {
   return new Date(date).toLocaleString('en-US', {
-    month: 'long',
-    day: '2-digit',
+    month: 'short',
     year: 'numeric',
   })
 }
@@ -14,7 +13,7 @@ export const postIsPublished = (post: any) => {
   return post.Published === 'Yes'
 }
 
-export const normalizeSlug = slug => {
+export const normalizeSlug = (slug) => {
   if (typeof slug !== 'string') return slug
 
   let startingSlash = slug.startsWith('/')
