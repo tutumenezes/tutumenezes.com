f03327acc622779cc2f1df4603d14149350c642e
tutumenezes <tutumenezes@hub9.co>
2022-03-30 20:41:48 -0300

    Added Analytcs Code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,30 @@ import '../styles/index.scss'
 import { ThemeProvider } from 'next-themes'
 import Layout from '../components/layout'
 
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gTag'
+import Analytics from '../components/analytics'
+
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <ThemeProvider>
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      <Analytics />
     </ThemeProvider>
   )
 }
