b282f45f219e3cbb34d667f012d1630015dd3cde
tutumenezes <tutumenezes@hub9.co>
2022-05-14 16:31:20 -0300

    New render oauth (#1)
    * NOTION SDK
    
    * fixed about page

--- src/lib/blog-helpers.ts
+++ src/lib/blog-helpers.ts
@@ -25,7 +25,7 @@ export const getDateStr = (date) => {
 }
 
 export const postIsPublished = (post: any) => {
-  return post.Published === 'Yes'
+  return post.Published
 }
 
 export const normalizeSlug = (slug) => {
