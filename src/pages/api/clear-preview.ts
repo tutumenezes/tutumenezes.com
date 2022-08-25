c4866006edeb101cb8f2d74b6cab16ddf720b9be
tutumenezes <tutumenezes@hub9.co>
2022-08-25 16:33:56 -0300

    updating APIS

--- src/pages/api/clear-preview.ts
+++ src/pages/api/clear-preview.ts
@@ -1,13 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.query.slug) {
-    res.clearPreviewData()
-    res.writeHead(307, { Location: `/${req.query.slug}` })
-    res.end()
-  } else {
-    res.clearPreviewData()
-    res.writeHead(307, { Location: `/` })
-    res.end()
-  }
+  res.clearPreviewData()
+  res.redirect('/')
 }
